440da85df08439de503114ec4f157c9632d44970
Brandon <Apex2.0predator@gmail.com>
2025-08-11 17:34:39 +0200

    updated firebase

--- TumaGo/firebase_init.py
+++ TumaGo/firebase_init.py
@@ -8,7 +8,7 @@ from firebase_admin import credentials
 
 def initialize_firebase():
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-    cred_path = os.path.join(BASE_DIR, 'firebase', 'delivery.json')
+    cred_path = os.path.join(BASE_DIR, 'delivery.json')
 
     if not firebase_admin._apps:
         cred = credentials.Certificate(cred_path)
